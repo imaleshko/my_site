6496bb873c3b9f65342722a1f0d354176b0b43f5
imaleshko <maleshko.ivan@student.uzhnu.edu.ua>
2024-11-04 22:53:57 +0200

    lab10

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-alert("На цій сторінці ви можете купити квиток на понеділок");
+function openForm() {
+    const form = document.getElementById("messageForm");
+    if (form.style.display === "none") {
+        form.style.display = "block";
+    }
+    else {
+        form.style.display = "none";
+    }
+}
 
-function onClick() {
-    confirm('Підтвердіть покупку білету')
+function sendMessage() {
+    const message = document.getElementById("messageInput").value;
+
+    if (message === "") {
+        alert("Будь ласка, введіть повідомлення перед відправкою.");
+        return;
+    }
+    else {
+        alert("Ваше повідомлення відправлено: " + message);
+        document.getElementById("messageInput").value = "";
+        openForm();
+    }
 }
